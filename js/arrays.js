ca8efa7d29e36b31f8ef06b2580b071d6de4d778
Ramesh Paudel <rpaudel58@gmail.com>
2019-07-15 14:59:32 +0545

    Reducing arrays

--- js/arrays.js
+++ js/arrays.js
@@ -12,3 +12,21 @@ var filteredData = test.filter(function(value,index){
     }
     return false
 })
+
+
+var arr= [1,2,3,4,5,6,7,8,9];
+
+
+
+function sumOfAllNo(first,second){
+    return first + second;
+}
+
+var results = arr.reduce(sumOfAllNo)
+
+
+var strArr= ["Hello","Bonjur","Hola"];
+
+var strResults = strArr.reduce(function(a,b){
+    return a+b;
+})
