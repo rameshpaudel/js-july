ba90a8d5f65de8e99ba1159f8f975ba15581f8c0
Ramesh Paudel <rpaudel58@gmail.com>
2019-07-23 16:25:15 +0545

    Added dom methods and basic styling

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+var container = document.getElementsByClassName('container');
+var header = document.getElementById("header")
+
+var allDiv = document.getElementsByTagName('div')
+var allHeadings = document.getElementsByTagName('h1')
+
+
+var firstContainer = document.querySelector(".container")
+var firstHeader = document.querySelector("#header")
+
+
+var firstDiv = document.querySelector('div');
+
+var allDivs = document.querySelectorAll('div');
+
+
+firstDiv.style = "background-color:#ededed; color: green"
+
+
+
+var paragraph = document.querySelector('p')
+
+// allDivs[0].style = 'text-tcxransform: uppercase'
