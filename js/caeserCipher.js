367215f730df644cc304d3fbf9dcf4498551a0f8
Ramesh Paudel <rpaudel58@gmail.com>
2019-07-24 15:15:16 +0545

    Fixed index position of caeser cipher

--- js/caeserCipher.js
+++ js/caeserCipher.js
@@ -1,14 +1,20 @@
+var alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
+
 function encryptData(myValue,rounds = 2){
-    var alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
+    
     var result = "";
 
     for(var i = 0; i < myValue.length; i++){
         //Converting the current character to lower case
         let currentCharacter = myValue[i].toLowerCase();
+        let characterIndexCount = alphabets.length -1;
         let currentIndex = alphabets.indexOf(currentCharacter);
-    
+        
         let finalIndex = currentIndex + rounds;
         
+        if(finalIndex > characterIndexCount){
+            finalIndex  = finalIndex - alphabets.length 
+        } 
         let finalCharacter = alphabets[finalIndex];
 
         if(myValue[i] == myValue[i].toUpperCase()){
@@ -18,30 +24,45 @@ function encryptData(myValue,rounds = 2){
         
     
     }
-    console.log("The input was ", myValue, ' and the encrypted text is ', result)    
+    // console.log("The input was ", myValue, ' and the encrypted text is ', result)    
     return result;
 }
 
 
 function decryptData(myValue, rounds = 2){
-    var alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
     var result = "";
 
     for(var i = 0; i < myValue.length; i++){
         //Converting the current character to lower case
         let currentCharacter = myValue[i].toLowerCase();
+
+        let characterIndexCount = alphabets.length -1;
+
         let currentIndex = alphabets.indexOf(currentCharacter);
-    
+
         let finalIndex = currentIndex - rounds;
+      
+        if(finalIndex < 0){
+            finalIndex  =   alphabets.length + finalIndex
+        } 
+
         let finalCharacter = alphabets[finalIndex];
+
 
         if(myValue[i] == myValue[i].toUpperCase()){
             finalCharacter = finalCharacter.toUpperCase()
         }
+        
         result = result + finalCharacter;
     
     }
-    console.log("The input was ", myValue, ' and the encrypted text is ', result)    
     return result;
 }
 
+
+
+var enc = "bbbzzzaeee"
+var dec = "zzzxxxyccc"
+
+var encrypted = encryptData(enc);
+var decrypted = decryptData(encrypted);
